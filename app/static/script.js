d309625d365b562c941d57efd1b6c76d6a98a545
juanmontenegro58 <juanmontenegro58@gmail.com>
2022-10-23 21:47:50 -0500

    - Nueva visualización del feed de instagram. - Se quita el texto Rattan & Rattan y se coloca el logo.

--- app/static/script.js
+++ app/static/script.js
@@ -5,18 +5,17 @@ const fetchData = async () => {
 
     const response = await fetch(url)
     const data = await response.json()
-    let content = '<div class"row">'
+    let content = ''
     for(let item of data.data) {
         const mediaType = item.media_type
         if(mediaType === 'IMAGE'){
-            content += `<div class="col-md-6">
+            content += `<div class="column">
                 <a href="${item.permalink}" target="_blank">
-                <img src="${item.media_url}" class="dimensiones_ig" alt="...">
+                <img src="${item.media_url}" class="dimensiones_ig image" alt="...">
                 </a>
             </div>`
         }
     }
-    content += '</div>'
     const galeria_port = document.getElementById('galeria_port')
     galeria_port.innerHTML = content
 }
